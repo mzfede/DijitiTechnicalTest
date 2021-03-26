0f3fc006857ddc0a48eda3aa8342229e6e479884
Federico Mazza <federico.mazza97@hotmail.it>
2021-03-26 15:06:20 +0100

    test

--- dijiti/src/Controller/DijitiController.php
+++ dijiti/src/Controller/DijitiController.php
@@ -1,34 +1,43 @@
 <?php
-  // src/Controller/LuckyController.php
+
   namespace App\Controller;
   use PDO;
   use Symfony\Component\HttpFoundation\Response;
   use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
   //use Symfony\Component\Routing\Annotation\Route;
   //require("dbConnect.php");
-    try {
-        $hostname = "127.0.0.1";
-        $dbname = "dijiti";
-        $user = "root";
-        $pass = "root";
-        $db = new PDO ("mysql:host=$hostname;dbname=$dbname", $user, $pass);
-    } catch (PDOException $e) {
-        echo "Errore: " . $e->getMessage();
-    die();
-    }
-    $res = $db->query("SELECT * FROM user");
-    $rows = $res->fetchAll(PDO::FETCH_ASSOC);
 
   class DijitiController extends AbstractController
   {
-      public function number($rows): Response
+      public function number(): Response
       {
-        print json_encode($rows);
-          $number = random_int(0, 100);
+        try {
+            $hostname = "127.0.0.1";
+            $dbname = "dijiti";
+            $user = "root";
+            $pass = "root";
+            $db = new PDO ("mysql:host=$hostname;dbname=$dbname", $user, $pass);
+        } catch (PDOException $e) {
+            echo "Errore: " . $e->getMessage();
+        die();
+        }
+        $tmp = array();
+        $toSend = array();
+        $firstRow = array();
+        $res = $db->query("SELECT * FROM user");
+        $rows = $res->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($rows as $value) {
+          foreach ($value as $e) {
+            array_push($tmp, $e);
+          }
+          array_push($toSend, $tmp);
+          $tmp = array();
+        }
+        $firstRow = array_shift($toSend);
 
-          return $this -> render('number.html.twig',[
-            'number' => $number,
-          ]);
+        return $this -> render('number.html.twig',[
+          'tableHead' => $firstRow, 'sent' => $toSend,
+        ]);
       }
 
       public function text(): Response
