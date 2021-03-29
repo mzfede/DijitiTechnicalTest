f72d07e9daea05eb7dd99d9a5a505b65551ad91b
Federico Mazza <federico.mazza97@hotmail.it>
2021-03-29 17:43:15 +0200

    test

--- dijiti/src/Controller/DijitiController.php
+++ dijiti/src/Controller/DijitiController.php
@@ -11,39 +11,55 @@
   {
       public function index(): Response
       {
-        try {
-            $hostname = "127.0.0.1";
-            $dbname = "dijiti";
-            $user = "root";
-            $pass = "root";
-            $db = new PDO ("mysql:host=$hostname;dbname=$dbname", $user, $pass);
-        } catch (PDOException $e) {
-            echo "Errore: " . $e->getMessage();
-        die();
-        }
-        $tmp = array();
-        $toSend = array();
-        $firstRow = array();
-        $res = $db->query("SELECT * FROM user");
-        if ($res) {
-          $res = $res->fetchAll(PDO::FETCH_ASSOC);
-        }
-        else{  
-          print_r($res);
-          $res = array();
-        }
-        foreach ($res as $value) {
-          foreach ($value as $e) {
-            array_push($tmp, $e);
+          try {
+              $hostname = "127.0.0.1";
+              $dbname = "dijiti";
+              $user = "root";
+              $pass = "root";
+              $db = new PDO ("mysql:host=$hostname;dbname=$dbname", $user, $pass);
+          } catch (PDOException $e) {
+              echo "Errore: " . $e->getMessage();
+          die();
           }
-          array_push($toSend, $tmp);
-          $tmp = array();
+          if(isset($_POST['orderBy'])){// caricamento ordinato in base al bottone cliccato
+            $toOrder = $_POST['orderBy'];
+            $tmp = array();
+            $toSend = array();
+            $firstRow = array();
+            $res = array();
+            $res = $db->query("SELECT * FROM `users` ORDER BY `users`.`$toOrder` ASC");
+            $res = $res->fetchAll(PDO::FETCH_ASSOC);
+            foreach ($res as $value) {
+              foreach ($value as $e) {
+                array_push($tmp, $e);
+              }
+              array_push($toSend, $tmp);
+              $tmp = array();
+            }
+            $firstRow = array_shift($toSend);
+            $db = null;
+            return $this -> render('index.html.twig',[
+              'tableHead' => $firstRow, 'sent' => $toSend,
+            ]);
+          }else{ // caricamento normale
+            $tmp = array();
+            $toSend = array();
+            $firstRow = array();
+            $res = $db->query("SELECT * FROM users");
+            $res = $res->fetchAll(PDO::FETCH_ASSOC);
+            foreach ($res as $value) {
+              foreach ($value as $e) {
+                array_push($tmp, $e);
+              }
+              array_push($toSend, $tmp);
+              $tmp = array();
+            }
+            $firstRow = array_shift($toSend);
+            $db = null;
+            return $this -> render('index.html.twig',[
+              'tableHead' => $firstRow, 'sent' => $toSend,
+            ]);
         }
-        $firstRow = array_shift($toSend);
-        $db = null;
-        return $this -> render('index.html.twig',[
-          'tableHead' => $firstRow, 'sent' => $toSend,
-        ]);
       }
 
       public function insert(): Response
@@ -58,7 +74,7 @@
             echo "Errore: " . $e->getMessage();
         die();
         }
-        if (isset($_POST['submit']))
+        if (isset($_POST['submitInsert']))
         {
           $nome = $_POST['nome'];
           $cognome = $_POST['cognome'];
@@ -67,7 +83,7 @@
           $password = $_POST['password'];
           $telefono = $_POST['telefono'];
         }
-        //$res = $db->query("INSERT INTO `dijiti`.`users` (`Nome`, `Cognome`, `Email`, `Username`, `Password`, `Numero di telefono`) VALUES ('$nome', '$cognome', '$email', '$username', '$password', '$telefono')");
+        $res = $db->query("INSERT INTO `dijiti`.`users` (`Nome`, `Cognome`, `Email`, `Username`, `Password`, `Numero`) VALUES ('$nome', '$cognome', '$email', '$username', '$password', '$telefono')");
         $db = null;
         return $this -> render('inserito.html.twig',[
           'nome' => $nome,
@@ -76,6 +92,37 @@
           'username' =>  $username,
           'password' =>  $password,
           'telefono' =>  $telefono,
+        ]);
+      }
+
+      public function delete(): Response
+      {
+        try {
+            $hostname = "127.0.0.1";
+            $dbname = "dijiti";
+            $user = "root";
+            $pass = "root";
+            $db = new PDO ("mysql:host=$hostname;dbname=$dbname", $user, $pass);
+        } catch (PDOException $e) {
+            echo "Errore: " . $e->getMessage();
+        die();
+        }
+        $users = array();
+        if (isset($_POST['submitDelete']))
+        {
+          if(!empty($_POST['toDelete'])) {
+            foreach($_POST['toDelete'] as $value){
+              array_push($users, $value);
+            }
+          }
+        }
+
+        foreach ($users as $e) {
+          $res = $db->query("DELETE FROM `dijiti`.`users` WHERE `Username` = '$e'");
+        }
+        $db = null;
+        return $this -> render('eliminato.html.twig',[
+          'usersToDelete' => $users,
         ]);
       }
   }
